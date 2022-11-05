aac8216e89e58f342672db0e8e9342502885337e
fernandaguedess <mariafsguedes@fac.pe.senac.br>
2022-11-05 15:22:43 -0300

    API consumida e funcionando

--- main.js
+++ main.js
@@ -1,32 +1,21 @@
-const resdiv = document.querySelector('#results');
-const resbtn = document.querySelector('#getdata');
-
-
 function get(url){
     let request = new XMLHttpRequest()
     request.open('GET', url, false)
     request.send()
     return request.responseText
+
 }
 
-
+//onclick="main(document.getElementById('advice').value)
 
 function main(){
-    //users = get(https://api.adviceslip.com/advice)
     data = get("https://api.adviceslip.com/advice")
-    advice = JSON.parse(getdata).slip;
-    getdata.innerHTML = `<p>${advice.advice}</p>`;
-    //console.log(advice.advice)
-    //console.log(advice.innerHTML = `<p>${advice.advice}</p>`)
+    advice = JSON.parse(data).slip;
+    alert(advice.advice)
     
-    //test = advice.slip
-    //console.log(test)
 }
 main()
 
 
 
 
-//getdata button
-//results div
-//return result.innerHTML
